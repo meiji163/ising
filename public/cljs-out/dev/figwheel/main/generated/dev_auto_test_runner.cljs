0a0c96076f3d90f45abad722a91e2cf9caaf745f
meiji163 <mysatellite99@gmail.com>
2022-09-05 21:45:32 -0700

    get the build to work FINALLY

--- public/cljs-out/dev/figwheel/main/generated/dev_auto_test_runner.cljs
+++
@@ -1,5 +0,0 @@
-(ns figwheel.main.generated.dev-auto-test-runner
-  (:require [cljs.test :refer [run-tests]]
-            [cljs-test-display.core] [ising.core-test]))
-
-  (run-tests (cljs-test-display.core/init! "app-auto-testing") (quote ising.core-test))
--- public/cljs-out/dev/ising/core_test.js
+++
@@ -1,6 +0,0 @@
-// Compiled by ClojureScript 1.11.4 {:optimizations :none}
-goog.provide('ising.core_test');
-goog.require('cljs.core');
-goog.require('cljs.test');
-
-//# sourceMappingURL=core_test.js.map
